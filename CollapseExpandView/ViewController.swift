9a5fd8ebd3c2d34ebe9b6c20cc7815c55b0cb55c
shkhan9 <shehatagamal120@yahoo.com>
2018-01-08 02:24:41 -0700

    first commit

---
+++ CollapseExpandView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollapseExpandView
+//
+//  Created by sameh mohammed on 1/8/18.
+//  Copyright © 2018 com.najeeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
